2af5253c683022f112991be961fe792ea93fcb1a
Lucas <lucas_bielefeldt@estudante.sc.senai.br>
2018-11-22 17:38:37 -0200

    add teste

---
+++ teste.php
@@ -0,0 +1,73 @@
+<style type="text/css"> 
+a:link
+{ 
+text-decoration:none; 
+font-size: 24px;
+color:black;
+font: arial;
+} 
+
+
+</style>
+
+<?php
+
+// URL DO SITE
+$url = 'http://www.ambientebrasil.com.br/';
+
+// PEGANDO TODO CONTEUDO
+$dadosSite = file_get_contents($url);
+
+// PEGANDO A IMAGEM
+$img1 = explode('class="mh-posts-large-thumb">
+',$dadosSite);
+$img2 = explode('></figure>',$img1[0]);
+
+// PEGANDO O TÍTULO
+$titulo1 = explode('<h3 class="entry-title mh-posts-large-title">
+',$dadosSite);
+$titulo2 = explode('</h3>',$titulo1[1]);
+
+// PEGANDO A DATA
+$data1 = explode('class="fa fa-clock-o">', $dadosSite);
+$data2 = explode('</i>', $data1[1]);
+
+// PEGANDO A DESCRIÇÃO
+$descri1 = explode('<p>',$dadosSite);
+$descri2 = explode('</p>',$descri1[1]);
+
+// ---------------------------------------------------- // 
+
+// PEGANDO A IMAGEM
+//$img3 = explode('<div class="mh-row mh-posts-digest-wrap mh-posts-digest-large clearfix"
+//',$dadosSite);
+//$img4 = explode('></div>',$img3[0]);
+
+// PEGANDO O TÍTULO
+$titulo3 = explode('<h3 class="mh-posts-digest-title mh-posts-digest-title-large">
+',$dadosSite);
+$titulo4 = explode('</h3>',$titulo3[1]);
+
+// PEGANDO O TÍTULO
+$titulo5 = explode('<h3 class="mh-posts-digest-title mh-posts-digest-title-large">
+',$dadosSite);
+$titulo6 = explode('</h3>',$titulo5[1]);
+
+// ---------------------------------------------------- // 
+
+// IMPRIMIR NA TELA
+echo "<br><br>";
+echo "<table>
+
+  <tr>
+  		<td><h3>".$img2[0]."<h3><h2>".$titulo2[0]."</h2><h5>".$descri2[0]."</h5><br><h2>".$titulo4[0]."</h2><br></td>
+  		
+  	</tr>	
+  	
+  	<tr>	
+  	
+  
+  	</table>";
+
+ 
+?>
