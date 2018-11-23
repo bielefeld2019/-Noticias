109df25dc469bdb6409970b60a52de236717977a
Lucas <lucas_bielefeldt@estudante.sc.senai.br>
2018-11-23 16:30:19 -0200

    arquivos dia 23/11

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ color:black;
 font: arial;
 } 
 
-
 </style>
 
 <?php
@@ -19,10 +18,15 @@ $url = 'http://www.ambientebrasil.com.br/';
 $dadosSite = file_get_contents($url);
 
 // PEGANDO A IMAGEM
-$img1 = explode('class="mh-posts-large-thumb">
+$logo = explode('class="mh-posts-large-thumb">
 ',$dadosSite);
-$img2 = explode('></figure>',$img1[0]);
+$logo2 = explode('></figure>',$logo[0]);
 
+// PEGANDO A IMAGEM
+$img1 = explode('<div class="mh-image-caption mh-posts-large-caption"
+>',$dadosSite);
+$img2 = explode('</div>',$img1[0]);
+class="mh-widget mh-home-2 mh-widget-col-2 mh_magazine_posts_large"
 // PEGANDO O TÍTULO
 $titulo1 = explode('<h3 class="entry-title mh-posts-large-title">
 ',$dadosSite);
@@ -34,34 +38,38 @@ $descri2 = explode('</p>',$descri1[1]);
 
 // ---------------------------------------------------- // 
 
-// PEGANDO A IMAGEM
-// $img3 = explode('class="mh-posts-large-thumb">
-//',$dadosSite);
-//$img4 = explode('</figure>',$img3[0]);
-
-// PEGANDO O TÍTULO
+// PEGANDO O TÍTULO ---X--- 2 NOTICIAS COM IMAGEM E TITULO
 $titulo3 = explode('<h3 class="mh-posts-digest-title mh-posts-digest-title-large">
 ',$dadosSite);
-$titulo4 = explode('</h3>',$titulo3[1]);
+$titulo4 = explode('</h3>', $titulo3[1]);
 
-// PEGANDO O TÍTULO
-$titulo5 = explode('<h3 class="mh-posts-digest-title mh-posts-digest-title-large">
-',$dadosSite);
+// PEGANDO O TÍTULO 3 NOTICIAS COM SOMENTE O TITULO 
+$titulo5 = explode('<h3 class="mh-posts-digest-title mh-posts-digest-title-small">', $dadosSite);
 $titulo6 = explode('</h3>',$titulo5[1]);
+
+// ----- 4
+// PEGANDO O TÍTULO ---X---  2 NOTICIAS COMPLETAS (TITULO, DESCRIÇÃO E IMAGEM) NA VERTICAL 
+$titulo7 = explode('<h3 class="entry-title mh-posts-list-title">
+', $dadosSite);
+$titulo8 = explode('</h3>',$titulo7[1]);
+
+// PEGANDO A DESCRIÇÃO
+$descri7 = explode('<div class="mh-posts-list-excerpt clearfix">
+',$dadosSite);
+$descri8 = explode('</div>',$descri7[1]);
+
 
 // ---------------------------------------------------- // 
 
 // IMPRIMIR NA TELA
 echo "<br><br>";
-echo "<table>
+echo "<table width='100'>
 
   <tr>
-  		<td><h3>".$img2[0]."<h3><h2>".$titulo2[0]."</h2><h5>".$descri2[0]."</h5></td>
+  		<td>".$logo[0]."<br><h2><strong>".$titulo2[0]."</strong></h2><h5>".$descri2[0]."</h5><br><h2>".$titulo4[0]."</h2><br><h4>".$titulo6[0]."</h4><br><h4>".$titulo8[0]."</h4><h5>".$descri8[0]."</h5></td>
   		
   	</tr>	
-  	<tr>	
-  	
-  
+
   	</table>";
 
  
--- teste.php
+++ teste.php
@@ -7,7 +7,6 @@ color:black;
 font: arial;
 } 
 
-
 </style>
 
 <?php
@@ -19,18 +18,19 @@ $url = 'http://www.ambientebrasil.com.br/';
 $dadosSite = file_get_contents($url);
 
 // PEGANDO A IMAGEM
-$img1 = explode('class="mh-posts-large-thumb">
+$logo = explode('class="mh-posts-large-thumb">
 ',$dadosSite);
-$img2 = explode('></figure>',$img1[0]);
+$logo2 = explode('></figure>',$logo[0]);
+
+// PEGANDO A IMAGEM
+$img1 = explode('<div class="mh-image-caption mh-posts-large-caption"
+>',$dadosSite);
+$img2 = explode('</div>',$img1[0]);
 
 // PEGANDO O TÍTULO
 $titulo1 = explode('<h3 class="entry-title mh-posts-large-title">
 ',$dadosSite);
 $titulo2 = explode('</h3>',$titulo1[1]);
-
-// PEGANDO A DATA
-$data1 = explode('class="fa fa-clock-o">', $dadosSite);
-$data2 = explode('</i>', $data1[1]);
 
 // PEGANDO A DESCRIÇÃO
 $descri1 = explode('<p>',$dadosSite);
@@ -38,35 +38,41 @@ $descri2 = explode('</p>',$descri1[1]);
 
 // ---------------------------------------------------- // 
 
-// PEGANDO A IMAGEM
-//$img3 = explode('<div class="mh-row mh-posts-digest-wrap mh-posts-digest-large clearfix"
-//',$dadosSite);
-//$img4 = explode('></div>',$img3[0]);
-
-// PEGANDO O TÍTULO
+// PEGANDO O TÍTULO ---X--- PERGUNTAR COMO PEGAR AS 2 NOTICIAS COMO ELAS TEM AS MESMAS CLASSES E ETC
 $titulo3 = explode('<h3 class="mh-posts-digest-title mh-posts-digest-title-large">
 ',$dadosSite);
-$titulo4 = explode('</h3>',$titulo3[1]);
+$titulo4 = explode('</h3>', $titulo3[1]);
 
-// PEGANDO O TÍTULO
-$titulo5 = explode('<h3 class="mh-posts-digest-title mh-posts-digest-title-large">
-',$dadosSite);
+// PEGANDO O TÍTULO PERGUNTAR COMO PEGAR AS 2 NOTICIAS COMO ELAS TEM AS MESMAS CLASSES E ETC
+$titulo5 = explode('<h3 class="mh-posts-digest-title mh-posts-digest-title-small">', $dadosSite);
 $titulo6 = explode('</h3>',$titulo5[1]);
+
+// ----- 4
+// PEGANDO O TÍTULO ---X---  PERGUNTAR COMO PEGAR AS 2 NOTICIAS COMO ELAS TEM AS MESMAS CLASSES E ETC
+$titulo7 = explode('<h3 class="entry-title mh-posts-list-title">
+', $dadosSite);
+$titulo8 = explode('</h3>',$titulo7[1]);
+
+// PEGANDO A DESCRIÇÃO
+$descri7 = explode('<div class="mh-posts-list-excerpt clearfix">
+',$dadosSite);
+$descri8 = explode('</div>',$descri7[1]);
+
+// PEGANDO A IMAGEM
+$img7 = explode('<img width="678" height="381" src="http://noticias.ambientebrasil.com.br/wp-content/uploads/2018>"',$dadosSite);
+$img8 = explode('/>',$img7[0]);
 
 // ---------------------------------------------------- // 
 
 // IMPRIMIR NA TELA
 echo "<br><br>";
-echo "<table>
+echo "<table width:1000; weigth:700; margin-top:0;>
 
   <tr>
-  		<td><h3>".$img2[0]."<h3><h2>".$titulo2[0]."</h2><h5>".$descri2[0]."</h5><br><h2>".$titulo4[0]."</h2><br></td>
+  		<td>".$logo[0]."<h2><strong>".$titulo2[0]."</strong></h2><h5>".$descri2[0]."</h5><br><h2>".$titulo4[0]."</h2><br><h4>".$titulo6[0]."</h4><br><h4>".$titulo8[0]."</h4><h5>".$descri8[0]."</h5></td>
   		
   	</tr>	
-  	
-  	<tr>	
-  	
-  
+
   	</table>";
 
  
